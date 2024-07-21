380f4150f71818a7605638d45b22a32329913d0b
Vidhi Yadav <79339896+vyadav5@users.noreply.github.com>
2024-07-21 15:50:49 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
-variable "access_key" {}
-variable "secret_key" {}
+#variable "access_key" {}
+#variable "secret_key" {}
 variable "awsRegion" {}
 variable "instanceType" {}
 variable "instanceName" {}
